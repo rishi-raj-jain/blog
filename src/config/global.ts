d39035ec2ad0707439b345abfc15e94f7b0141f7
CaptainOfPhB <captainofphb@gmail.com>
2023-12-18 08:37:05 +0800

    chore: update

---
+++ src/config/global.ts
@@ -0,0 +1,20 @@
+export const SiteConfig = {
+  author: {
+    name: 'CaptainOfPhB',
+    email: 'captainofphb#gmail.com'
+  },
+  title: 'CaptainOfPhB',
+  description: '船长的部落格，记录有趣的事，分享技术经验',
+  navs: [
+    { title: 'Home', url: '/' },
+    { title: 'Blogs', url: '/blogs' },
+    { title: 'Projects', url: '/project' },
+    { title: 'About', url: '/about' },
+  ],
+  socialMedias: [
+    { title: 'GitHub', url: 'https://github.com/CaptainOfPhB' },
+    { title: 'X(Twitter)', url: 'https://twitter.com/CaptainOfPhB' },
+    { title: '知乎', url: 'https://www.zhihu.com/people/CaptainInPHW' },
+    { title: '微信', url: 'https://picx.zhimg.com/80/v2-502fce7fabff98705dfab84cc58bb31d_1440w.png' },
+  ]
+}
