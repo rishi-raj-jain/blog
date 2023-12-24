034d3186595b3929648684fe0e8f8002acea701e
CaptainOfPhB <captainofphb@gmail.com>
2023-12-24 22:33:34 +0800

    chore: update

--- plugins/remark-modified-time.mjs
+++ plugins/remark-modified-time.mjs
@@ -4,7 +4,7 @@ import { execSync } from "child_process";
 export function remarkModifiedTime() {
   return function (tree, file) {
     const filepath = file.history[0];
-    const output = execSync(`git --no-pager log -1 --pretty=%cI "${filepath}"`);
-    file.data.astro.frontmatter.lastModified = output.toString().replace('\n', '');
+    const output = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
+    file.data.astro.frontmatter.lastModified = output.toString();
   };
 }
