e00a4dd8815440b9f34d384f908338d98c0dbc3e
CaptainOfPhB <captainofphb@gmail.com>
2023-12-26 16:18:07 +0800

    chore: install @vercel/analytics

--- plugins/remark-modified-time.mjs
+++ plugins/remark-modified-time.mjs
@@ -1,8 +1,7 @@
-import process from "process";
 import { execSync } from "child_process";
 
 export function remarkModifiedTime() {
-  return function (tree, file) {
+  return function (, file) {
     const filepath = file.history[0];
     const output = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
     file.data.astro.frontmatter.lastModified = output.toString();
