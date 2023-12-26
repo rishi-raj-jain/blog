38d3f6e8f642d614f161ae32d49eaf66aa47142f
CaptainOfPhB <captainofphb@gmail.com>
2023-12-26 16:21:23 +0800

    chore: update

--- plugins/remark-modified-time.mjs
+++ plugins/remark-modified-time.mjs
@@ -1,7 +1,7 @@
 import { execSync } from "child_process";
 
 export function remarkModifiedTime() {
-  return function (, file) {
+  return function (tree, file) {
     const filepath = file.history[0];
     const output = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
     file.data.astro.frontmatter.lastModified = output.toString();
